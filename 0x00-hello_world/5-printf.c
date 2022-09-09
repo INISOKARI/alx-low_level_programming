95dcbab67f6e2129f43215305ef8a8e09038066d
INISOKARI <mails2neeso@gmail.com>
2022-09-08 19:35:42 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,10 @@
 include <stdio.h>
-
 /**
-* main - print the string in the put function
-*
-* Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle
+* main - entry point
 * Return: 0
 */
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+ printf("with proper grammar, but the outcome is a piece of art,\n");
+ return (0);
 }
