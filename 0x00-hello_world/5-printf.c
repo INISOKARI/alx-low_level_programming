203fb460308df09e1d218e99edc4937001697ae9
INISOKARI <mails2neeso@gmail.com>
2022-09-08 20:59:29 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
-include <stdio.h>
-
+touch 5-printf.c
+#include <stdio.h>
 /**
- * main - Prints a text
- *
+ * main - main block
  * Return: 0
  */
 int main(void)
 {
-     printf("with proper grammar, but the outcome is a piece of art,\n");
-     return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
