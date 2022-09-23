69b73998fe50e1132e77220ed1f103abef00bb38
INISOKARI <mails2neeso@gmail.com>
2022-09-23 14:37:05 -0700

    5Print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,6 +6,6 @@ touch 5-printf.c
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
